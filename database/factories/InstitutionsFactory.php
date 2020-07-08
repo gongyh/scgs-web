6b92dd1e6639a05faac8765851cb46ee05207e59
gongyh <gongyh@qibebt.ac.cn>
2020-07-08 17:31:17 +0800

    generate fake data for institutions table

---
+++ database/factories/InstitutionsFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Institutions;
+use Faker\Generator as Faker;
+
+$factory->define(Institutions::class, function (Faker $faker) {
+    return [
+        'name' => $faker->address,
+        'updated_at' => now(),
+        'created_at' => now()
+    ];
+});
