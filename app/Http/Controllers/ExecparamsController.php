4280a5b08261f10b54293bbd2956886e7ac8abb7
zhoushiqi <13854291622@163.com>
2020-09-22 17:06:26 +0800

    laravel Controller process ajax request

--- app/Http/Controllers/ExecparamsController.php
+++ app/Http/Controllers/ExecparamsController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Artisan;
 use App\Execparams;
 use App\pipelineParams;
 use App\Status;
@@ -17,6 +17,79 @@ class ExecparamsController extends Controller
     //
     public function index(Request $request)
     {
+        if ($request->isMethod('POST')) {
+            $samples = new Samples();
+            $pipelineParams = pipelineParams::find(1);
+            $sample_id = $request->input('sampleID');
+            $ass = $request->input('ass') == 'ass' ? true : false;
+            $snv = $request->input('snv') == 'snv' ? true : false;
+            $cnv = $request->input('cnv') == 'cnv' ? true : false;
+            $genus = $request->input('genus') == 'genus' ? true : false;
+            $resfinder_db = $request->input('resfinder_db') == 'resfinder_db' ? true : false;
+            $nt_db = $request->input('nt_db') == 'nt_db' ? true : false;
+            $eggnog = $request->input('eggnog_db') == 'eggnog_db' ? true : false;
+            $kraken_db = $request->input('kraken_db') == 'kraken_db' ? true : false;
+            $kofam_profile = $request->input('kofam_profile') == 'kofam_profile' ? true : false;
+            $kofam_kolist = $request->input('kofam_kolist') == 'kofam_kolist' ? true : false;
+            if ($request->input('genus') != null) {
+                $this->validate($request, [
+                    'genus_name' => 'required|max:200'
+                ]);
+                $genus_name = $request->input('genus_name');
+            } else {
+                $genus_name = null;
+            }
+            if (Execparams::where('samples_id', $sample_id)->get()->count() == 0) {
+                Execparams::create([
+                    'samples_id' => $sample_id,
+                    'ass' => $ass,
+                    'snv' => $snv,
+                    'cnv' => $cnv,
+                    'genus' => $genus,
+                    'genus_name' => $genus_name,
+                    'resfinder_db' => $resfinder_db,
+                    'nt_db' => $nt_db,
+                    'eggnog' => $eggnog,
+                    'kraken_db' => $kraken_db,
+                    'kofam_profile' => $kofam_profile,
+                    'kofam_kolist' => $kofam_kolist
+                ]);
+            } else {
+                $id = Execparams::where('samples_id', $sample_id)->value('id');
+                $execparams = Execparams::find($id);
+                $execparams->ass = $ass;
+                $execparams->snv = $snv;
+                $execparams->cnv = $cnv;
+                $execparams->genus = $genus;
+                $execparams->genus_name = $genus_name;
+                $execparams->resfinder_db = $resfinder_db;
+                $execparams->kraken_db = $kraken_db;
+                $execparams->nt_db = $nt_db;
+                $execparams->eggnog = $eggnog;
+                $execparams->kofam_profile = $kofam_profile;
+                $execparams->kofam_kolist = $kofam_kolist;
+                $execparams->save();
+            }
+            RunPipeline::dispatch($sample_id);
+
+            // Status表
+            $start = time();
+            $user_id = Auth::user()->id;
+            if (Status::where('sample_id', $sample_id)->get()->count() == 0) {
+                Status::create([
+                    'user_id' => $user_id,
+                    'sample_id' => $sample_id,
+                    'started' => $start,
+                    'status' => false
+                ]);
+            } else {
+                $status_id = Status::where('sample_id', $sample_id)->value('id');
+                $status = Status::find($status_id);
+                $status->started = $start;
+                $status->save();
+            }
+            return redirect('/execute/start?sampleID=' . $sample_id);
+        }
         $pipelineParams = pipelineParams::find(1);
         $sample_id = $request->input('sampleID');
         if (Execparams::where('samples_id', $sample_id)->get()->count() != 0) {
@@ -42,76 +115,33 @@ class ExecparamsController extends Controller
 
     public function start(Request $request)
     {
-        $samples = new Samples();
         $pipelineParams = pipelineParams::find(1);
+        $samples = new Samples();
         $sample_id = $request->input('sampleID');
-        $ass = $request->input('ass') == 'ass' ? true : false;
-        $snv = $request->input('snv') == 'snv' ? true : false;
-        $cnv = $request->input('cnv') == 'cnv' ? true : false;
-        $genus = $request->input('genus') == 'genus' ? true : false;
-        $resfinder_db = $request->input('resfinder_db') == 'resfinder_db' ? true : false;
-        $nt_db = $request->input('nt_db') == 'nt_db' ? true : false;
-        $eggnog = $request->input('eggnog_db') == 'eggnog_db' ? true : false;
-        $kraken_db = $request->input('kraken_db') == 'kraken_db' ? true : false;
-        $kofam_profile = $request->input('kofam_profile') == 'kofam_profile' ? true : false;
-        $kofam_kolist = $request->input('kofam_kolist') == 'kofam_kolist' ? true : false;
-        if ($request->input('genus') != null) {
-            $this->validate($request, [
-                'genus_name' => 'required|max:200'
-            ]);
-            $genus_name = $request->input('genus_name');
-        } else {
-            $genus_name = null;
-        }
-        if (Execparams::where('samples_id', $sample_id)->get()->count() == 0) {
-            Execparams::create([
-                'samples_id' => $sample_id,
-                'ass' => $ass,
-                'snv' => $snv,
-                'cnv' => $cnv,
-                'genus' => $genus,
-                'genus_name' => $genus_name,
-                'resfinder_db' => $resfinder_db,
-                'nt_db' => $nt_db,
-                'eggnog' => $eggnog,
-                'kraken_db' => $kraken_db,
-                'kofam_profile' => $kofam_profile,
-                'kofam_kolist' => $kofam_kolist
-            ]);
-        } else {
-            $id = Execparams::where('samples_id', $sample_id)->value('id');
-            $execparams = Execparams::find($id);
-            $execparams->ass = $ass;
-            $execparams->snv = $snv;
-            $execparams->cnv = $cnv;
-            $execparams->genus = $genus;
-            $execparams->genus_name = $genus_name;
-            $execparams->resfinder_db = $resfinder_db;
-            $execparams->kraken_db = $kraken_db;
-            $execparams->nt_db = $nt_db;
-            $execparams->eggnog = $eggnog;
-            $execparams->kofam_profile = $kofam_profile;
-            $execparams->kofam_kolist = $kofam_kolist;
-            $execparams->save();
-        }
-        RunPipeline::dispatch($sample_id);
-
-        // Status表
-        $start = time();
-        $user_id = Auth::user()->id;
-        if (Status::where('sample_id', $sample_id)->get()->count() == 0) {
-            Status::create([
-                'user_id' => $user_id,
-                'sample_id' => $sample_id,
-                'started' => $start,
-                'status' => false
-            ]);
-        } else {
-            $status_id = Status::where('sample_id', $sample_id)->value('id');
-            $status = Status::find($status_id);
-            $status->started = $start;
-            $status->save();
-        }
+        $data = Execparams::where('samples_id', $sample_id);
+        $ass = $data->value('ass');    //boolean
+        $cnv = $data->value('cnv');    //boolean
+        $snv = $data->value('snv');    //boolean
+        $genus = $data->value('genus');     //boolean
+        $genus_name = $data->value('genus_name');    //string
+        $resfinder_db = $data->value('resfinder_db');     //boolean
+        $nt_db = $data->value('nt_db');     //boolean
+        $kraken_db = $data->value('kraken_db');     //boolean
+        $eggnog = $data->value('eggnog');    //boolean
+        $kofam_profile = $data->value('kofam_profile');    //boolean
+        $kofam_kolist = $data->value('kofam_kolist');     //boolean
         return view('Pipeline.pipelineStart', compact('samples', 'ass', 'cnv', 'snv', 'genus', 'genus_name', 'resfinder_db', 'nt_db', 'kraken_db',  'eggnog',  'kofam_profile', 'kofam_kolist', 'sample_id', 'pipelineParams'));
+    }
+
+    public function ajax(Request $request)
+    {
+        $run_sample_user = $request->input('run_sample_user');
+        $nextflow_log_path = $run_sample_user . '/.nextflow.log';
+        if (Storage::disk('local')->exists($nextflow_log_path)) {
+            $data = Storage::get($nextflow_log_path);
+            return response()->json(['success' => true, 'data' => $data]);
+        } else {
+            return response()->json(['error' => true, 'data' => 'can not read .nextflow.log!']);
+        }
     }
 }
