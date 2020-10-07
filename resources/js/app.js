5c50ee84b12fd24a3c0a2b4f242d4dfae0067d87
ShiqiZ <13854291622@163.com>
2020-10-07 15:51:14 +0800

    webpack mix update

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,8 @@
  */
 
 require('./bootstrap');
+require('./animate-bg');
+require('./common');
 
 window.Vue = require('vue');
 
@@ -28,5 +30,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app',
+  el: '#app',
 });
