573d4b0a892d7c1d4b883b2010d26717046acf72
zhoushiqi <13854291622@163.com>
2020-10-26 10:18:51 +0800

    .nextflow.log read bug fix

--- app/Http/Controllers/ExecparamsController.php
+++ app/Http/Controllers/ExecparamsController.php
@@ -267,9 +267,10 @@ class ExecparamsController extends Controller
 
     public function ajax(Request $request)
     {
-        $run_sample_user = $request->input('run_sample_user');
-        $run_user_id = User::where('name', $run_sample_user)->value('id');
-        $uuid = Jobs::where([['user_id', '=', $run_user_id], ['status', '=', 1]])->value('uuid');
+        $running_sample_id = $request->input('running_sample_id');
+        $uuid = Jobs::where([['sample_id', '=', $running_sample_id], ['status', '=', 1]])->value('uuid');
+        $user_id = Jobs::where('sample_id', $running_sample_id)->value('user_id');
+        $run_sample_user = User::where('id', $user_id)->value('name');
         $nextflow_log_path = $run_sample_user . '/' . $uuid . '/.nextflow.log';
         if (Storage::disk('local')->exists($nextflow_log_path)) {
             $data = Storage::get($nextflow_log_path);
