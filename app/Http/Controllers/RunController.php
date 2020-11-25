6d306153ace158b157c36eec51a23eeb40df0e84
zhoushiqi <13854291622@163.com>
2020-11-25 14:42:11 +0800

    project running

--- app/Http/Controllers/RunController.php
+++ app/Http/Controllers/RunController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use App\Jobs;
 use App\Samples;
+use App\Projects;
 use App\Execparams;
 
 class RunController extends Controller
@@ -14,11 +15,12 @@ class RunController extends Controller
     public function index()
     {
         $samples = new Samples();
+        $projects = new Projects();
         $jobs = new Jobs();
         $execparams = new Execparams();
         $user_id = Auth::user()->id;
         $now = time();
         $user_jobs = Jobs::where('user_id', $user_id)->get();
-        return view('Workspace.pipelineStatus', ['user_jobs' => $user_jobs, 'samples' => $samples, 'jobs' => $jobs, 'now' => $now, 'execparams' => $execparams]);
+        return view('Workspace.pipelineStatus', ['user_jobs' => $user_jobs, 'projects' => $projects, 'samples' => $samples, 'jobs' => $jobs, 'now' => $now, 'execparams' => $execparams]);
     }
 }
--- resources/views/Samples/samples.blade.php
+++ resources/views/Samples/samples.blade.php
@@ -63,8 +63,10 @@
                 <path fill-rule="evenodd" d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
               </svg>
             </a>
-            @if(DB::table('jobs')->where('project_id',$project->id)->count() > 0 && DB::table('jobs')->where('project_id',$project->id)->orderBy('id','desc')->value('status') == 3)
+            @if(DB::table('jobs')->where('project_id',$projectID)->count() > 0 && DB::table('jobs')->where('project_id',$projectID)->orderBy('id','desc')->value('status') == 3)
             <a href="/projsuccessRunning?projectID={{$projectID}}" class="mr-2 btn btn-success">Show Project Report </a>
+            @elseif(DB::table('jobs')->where('project_id',$projectID)->count() > 0 && DB::table('jobs')->where('project_id',$projectID)->value('status') == 1)
+            <a href="/executeProj/start?projectID={{$projectID}}" class="mr-2 btn btn-default">Show Pipeline Status</a>
             @endif
           </div>
           <div class="project_sample mt-3">
