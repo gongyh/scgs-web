3ee2075d16378527e272983e8b93d2066f017113
gongyh <gongyh@qibebt.ac.cn>
2020-11-30 22:55:21 +0800

    add the capacity to connect Mongodb

--- config/database.php
+++ config/database.php
@@ -91,6 +91,20 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'host' => env('MDB_HOST', '127.0.0.1'),
+            'port' => env('MDB_PORT', 27017),
+            'database' => env('MDB_DATABASE', 'homestead'),
+            'username' => env('MDB_USERNAME', 'homestead'),
+            'password' => env('MDB_PASSWORD', 'secret'),
+            'options' => [
+                // here you can pass more settings to the Mongo Driver Manager
+                // see https://www.php.net/manual/en/mongodb-driver-manager.construct.php under "Uri Options" for a list of complete parameters that you can use
+                'database' => env('MDB_AUTHENTICATION_DATABASE', 'admin'), // required with Mongo 3+
+            ],
+        ],
+
     ],
 
     /*
