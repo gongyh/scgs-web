168cbc8a3fe0530cc70e7831f551e05ed9c348d8
zhoushiqi <13854291622@163.com>
2021-01-19 16:52:15 +0800

    error message update

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,42 @@
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+    body {
+      background-color: #fff;
+      color: #636b6f;
+      font-family: 'Nunito', sans-serif;
+      font-weight: 100;
+    }
+
+    .full-height {
+      height: 100vh;
+    }
+
+    .flex-center {
+      align-items: center;
+      display: flex;
+      justify-content: center;
+    }
+
+    .position-ref {
+      position: relative;
+    }
+
+    .message {
+      font-size: 22px;
+      text-align: center;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="flex-center position-ref full-height">
+    <div class="message" style="padding: 10px;">
+      Sorry ! No Results Found </div>
+  </div>
+</body>
+
+</html>
