ca3b66326bbe1586957561dd398bd90946165f3a
zhoushiqi <13854291622@163.com>
2021-01-20 15:39:59 +0800

    exceptions handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,8 +50,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        if ($exception) {
-            return response()->view('errors.'.$exception->getStatusCode(), [],$exception->getStatusCode());
+        if ($this->isHttpException($exception)) {
+            if ($exception->getStatusCode() == 404) {
+                return response()->view('errors.' . '404', [], 404);
+            }
         }
         return parent::render($request, $exception);
     }
